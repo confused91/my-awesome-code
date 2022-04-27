f00b61ecaa0e78369ee8dfd4478f5ab74f0e4a84
Sharada Doddi <sharada_doddi@epam.com>
2022-04-27 20:01:41 +0530

    deleted sample file and made few changes

--- src/test/java/selenium/pages/FacebookHomePage.java
+++ src/test/java/selenium/pages/FacebookHomePage.java
@@ -1,15 +1,11 @@
 package selenium.pages;
 
-import com.gargoylesoftware.htmlunit.Page;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.*;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.util.List;
 
 
 public class FacebookHomePage {
